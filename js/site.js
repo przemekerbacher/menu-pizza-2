51a15697ddd0f64434d40e5f48d710884e197d51
unknown <przemyslaw.erbacher@gmail.com>
2020-03-24 09:24:09 +0100

    add coin effect Coin effect run when customer press arrow button and display order

--- js/site.js
+++ js/site.js
@@ -1,5 +1,7 @@
 const orderButton = document.querySelector(".basket-button");
 const order = document.querySelector(".order");
+const wrap = document.querySelector(".wrap");
+const menuItems = document.querySelectorAll(".menu-item");
 
 if (orderButton)
   orderButton.addEventListener("click", () => {
@@ -7,9 +9,22 @@ if (orderButton)
       orderButton.classList.add("active");
       order.classList.add("active");
       order.classList.remove("disabled");
+
+      wrap.classList.add("rotate");
+      wrap.classList.remove("rotate-back");
     } else {
       orderButton.classList.remove("active");
       order.classList.remove("active");
       order.classList.add("disabled");
+
+      wrap.classList.remove("rotate");
+      wrap.classList.add("rotate-back");
     }
+  });
+
+if (menuItems)
+  menuItems.forEach(item => {
+    item.addEventListener("click", () => {
+      alert();
+    });
   });
