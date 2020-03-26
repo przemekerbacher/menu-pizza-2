11cc04d025f819d2b89441cd661f177f7c73f90f
unknown <przemyslaw.erbacher@gmail.com>
2020-03-26 13:03:26 +0100

    change buttons size add effect after click on plus button

--- js/site.js
+++ js/site.js
@@ -1,7 +1,23 @@
 const extraButton = document.querySelector(".buttons .expand");
+const addToBasketButtons = document.querySelectorAll(".add-to-basket");
+
 if (extraButton) {
   extraButton.addEventListener("click", function() {
     const extraMenu = document.querySelector(".buttons");
     extraMenu.classList.toggle("active");
+  });
+}
+
+if (addToBasketButtons) {
+  addToBasketButtons.forEach(button => {
+    button.addEventListener("click", function(e) {
+      e.preventDefault();
+      const iconAdd = button.querySelector(".add");
+      const iconOk = button.querySelector(".ok");
+
+      button.classList.toggle("active");
+      if (iconAdd) iconAdd.classList.toggle("hide");
+      if (iconOk) iconOk.classList.toggle("hide");
+    });
   });
 }
